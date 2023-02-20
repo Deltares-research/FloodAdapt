95e7e14b5272a2b30990c1386cd8723ccfb34286
Frederique <frederique.degroen@deltares.nl>
2023-02-20 17:38:38 +0100

    Added projection.py and updated risk_driver.py and slr.py

--- flood_adapt/object_model/projection.py
+++ flood_adapt/object_model/projection.py
@@ -3,20 +3,19 @@ from pathlib import Path
 from typing import Union
 
 
-class RiskDriver:
+class Projection:
     def __init__(self, input_data: Union[str, Path, dict, None] = None) -> None:
-        self.name = None
+        self.name = ""
         self.toml_file = None
-        self.type = None
+        self.risk_drivers = []
         self.input_data = input_data
-    
+
     def read(self):
         if self.input_data:
             if isinstance(self.input_data, Path | str):
                 self.config = read_config(self.input_data)
             elif isinstance(self.input_data, dict):
                 self.config = self.input_data
-
-    def write(out_path):
-        write_config(out_path)
-
+    
+    def write(self):
+        write_config(self.toml_file)
--- flood_adapt/object_model/risk_drivers/slr.py
+++ flood_adapt/object_model/risk_drivers/slr.py
@@ -1,6 +1,15 @@
 from flood_adapt.object_model.risk_drivers.risk_driver import RiskDriver
 
+
 class SLR(RiskDriver):
-    def __init__(self, config_path) -> None:
-        super().__init__(config_path)
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.value = 0
+        self.type = "hazard"
+    
+    def read(self, config_path):
+        if config_path.exists():
+            read_config(self.config_path)
+        self.value = self.config["value"]
         
