496a4ab7d5a1b135a7c34e81d6d0da63db3313f9
tim-d-w <Tim.deWilde@Deltares.nl>
2023-02-21 16:24:59 +0100

    made subchecks of dictionaries, edited toml file

--- flood_adapt/object_model/SiteConfig.py
+++ flood_adapt/object_model/SiteConfig.py
@@ -11,23 +11,31 @@ class SiteConfig():
         if val.validate_existence_config_file(self.config_path):
             config = read_config(self.config_path)
 
-        #Add variables as attributes of the class
-        mandatory_attributes = ["name","long_name","lat","lon","csname","cstype","sfincs","slr","gui","return_periods","dem","fiat","aggregation","floodmap"]
+        #Keys of dictionaries
+        mandatory_attributes = ["name","long_name","lat","lon","sfincs","slr","risk","gui","dem","fiat"]
         non_mandatory_attributes = ["river","obs_station","tim"]
-        mandatory_sfincs = ["version","offshore_model","overland_model","datum_offshore_model","datum_overland_model","diff_datum_offshore_overland","tidal_components","ambient_air_pressure"]
+        mandatory_sfincs = ["csname","cstype","version","offshore_model","overland_model","datum_offshore_model","datum_overland_model","diff_datum_offshore_overland","tidal_components","ambient_air_pressure","floodmap_no_data_value","floodmap_units"]
         mandatory_slr = ["vertical_offset","relative_to_year"]
-        # mandatory_gui = ["tide_harmonic_amplitude","flooding_threshold","return_periods"]
-        # mandatory_dem = ["filename","units"]
-        # mandatory_fiat = ["indexfilename","exposure_crs"]
-        # mandatory_aggregation = ["shapefiles","field_names"]
-        # mandatory_floodmap = ["type","no_data_value","units"]
+        mandatory_risk = ["return_periods","flooding_threshold"]
+        mandatory_gui = ["tide_harmonic_amplitude"]
+        mandatory_dem = ["filename","units","indexfilename"]
+        mandatory_fiat = ["exposure_crs","aggregation_shapefiles","aggregation_field_names","floodmap_type"]
         if val.validate_content_config_file(config,self.config_path,mandatory_attributes):
             for attr in mandatory_attributes:
+                #Check if attributes have correct dictionary keys
                 if attr == "sfincs":
                     val.validate_content_config_file(config[attr],self.config_path,mandatory_sfincs)
                 elif attr == "slr":
                     val.validate_content_config_file(config[attr],self.config_path,mandatory_slr)
-                #Is this a good way to check attributes of a dict?
+                elif attr == "risk":
+                    val.validate_content_config_file(config[attr],self.config_path,mandatory_risk)
+                elif attr == "gui":
+                    val.validate_content_config_file(config[attr],self.config_path,mandatory_gui)
+                elif attr == "dem":
+                    val.validate_content_config_file(config[attr],self.config_path,mandatory_dem)
+                elif attr == "fiat":
+                    val.validate_content_config_file(config[attr],self.config_path,mandatory_fiat)
+                #Add the attributes
                 setattr(self,attr,config[attr])
 
             for attr in non_mandatory_attributes:
