8e743b7e40c4f47b3015416a741e7d68b39ab51d
tim-d-w <Tim.deWilde@Deltares.nl>
2023-02-21 16:34:25 +0100

    renamed site and added test script

---
+++ tests/test_site.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+test_database = Path().absolute() / 'tests' / 'test_database'
+
+def test_read_config():
+    from flood_adapt.object_model.io.config_io import read_config
+
+    test_toml = test_database / "charleston" / "input" / "events" / "extreme12ft" / "extreme12ft.toml"
+    assert test_toml.is_file()
+
+    config = read_config(test_toml)
+
+    assert config
+    assert isinstance(config, dict)
