3646b2906271665a8038e67e21379467e42ee080
Gundula Winter <gundulawinter@gmail.com>
2023-02-21 10:33:25 +0100

    Projections update and template for other classes

--- flood_adapt/object_model/projection.py
+++ flood_adapt/object_model/projection.py
@@ -4,11 +4,17 @@ from typing import Union
 
 
 class Projection:
-    def __init__(self, input_data: Union[str, Path, dict, None] = None) -> None:
+    def __init__(self, inputfile: str) -> None:
+        self.set_default()
+        self.inputfile = inputfile
+
+    def __init__(self):
+        self.set_default()
+
+    def set_default(self):
         self.name = ""
         self.config_file = None
         self.risk_drivers = []
-        self.input_data = input_data
 
     def set_name(self, value):
         self.name = value
@@ -16,17 +22,15 @@ class Projection:
     def set_risk_drivers(self):
         self.risk_drivers
 
-    def read(self):
-        if self.input_data:
-            if isinstance(self.input_data, Path | str):
-                config = read_config(self.input_data)
-            elif isinstance(self.input_data, dict):
-                config = self.input_data
-            
+    def load(self):
+        if self.inputfile:
+            if isinstance(self.inputfile, str):
+                config = read_config(self.inputfile)
+
             self.set_name(config["name"])
             self.set_risk_drivers(config[""])
     
-    def write(self):
-        write_config(self.config_file)
+    # def write(self):
+    #     write_config(self.config_file)
     
 
