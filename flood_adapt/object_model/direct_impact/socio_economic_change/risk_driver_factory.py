636a0fea943206d3dad18334d94e07476080dfda
tim-d-w <Tim.deWilde@Deltares.nl>
2023-02-22 15:02:16 +0100

    devided projections into socio economic and physical projections; updated imports according to new folder structure; updated test_object_model_projections

--- flood_adapt/object_model/direct_impact/socio_economic_change/risk_driver_factory.py
+++ flood_adapt/object_model/direct_impact/socio_economic_change/risk_driver_factory.py
@@ -1,13 +1,13 @@
-from flood_adapt.object_model.risk_drivers.slr import SLR
-from flood_adapt.object_model.risk_drivers.population_growth_existing import PopulationGrowthExisting
-from flood_adapt.object_model.risk_drivers.population_growth_new import PopulationGrowthNew
+from flood_adapt.object_model.direct_impact.socio_economic_change.economic_growth import EconomicGrowth
+from flood_adapt.object_model.direct_impact.socio_economic_change.population_growth_existing import PopulationGrowthExisting
+from flood_adapt.object_model.direct_impact.socio_economic_change.population_growth_new import PopulationGrowthNew
 
 
 class RiskDriverFactory:
     @staticmethod
     def get_risk_drivers(risk_driver: str):
-        if risk_driver == 'slr':
-            return SLR()
+        if risk_driver == 'economic_growth':
+            return EconomicGrowth()
         elif risk_driver == 'population_growth_existing':
             return PopulationGrowthExisting()
         elif risk_driver == 'population_growth_new':
--- flood_adapt/object_model/direct_impact/socio_economic_change/socio_economic_change.py
+++ flood_adapt/object_model/direct_impact/socio_economic_change/socio_economic_change.py
@@ -4,12 +4,10 @@ from flood_adapt.object_model.validate.config import validate_existence_config_f
 from flood_adapt.object_model.direct_impact.socio_economic_change.risk_driver_factory import RiskDriverFactory
 
 
-class Projection:
+class SocioEconomicChange:
     """The Projection class containing various risk drivers."""
-    def __init__(self, config_file: str = None) -> None:
+    def __init__(self) -> None:
         self.set_default()
-        if config_file:
-            self.config_file = config_file
 
     def set_default(self) -> None:
         self.name = ""
@@ -18,12 +16,9 @@ class Projection:
         self.mandatory_keys = ["name", "long_name"]
 
         # Set the default values for all risk drivers
-        self.slr = RiskDriverFactory.get_risk_drivers('slr')
         self.population_growth_existing = RiskDriverFactory.get_risk_drivers('population_growth_existing')
         self.population_growth_new = RiskDriverFactory.get_risk_drivers('population_growth_new')
         self.economic_growth = RiskDriverFactory.get_risk_drivers('economic_growth')
-        self.precipitation_intensity = RiskDriverFactory.get_risk_drivers('precipitation_intensity')
-        self.storminess = RiskDriverFactory.get_risk_drivers('storminess')
 
     def set_name(self, value: str) -> None:
         self.name = value
@@ -33,9 +28,6 @@ class Projection:
     
     def set_risk_drivers(self, config: dict) -> None:
         # Load all risk drivers
-        if "sea_level_rise" in config.keys():
-            self.slr.load(config, self.config_file)
-
         if "population_growth_existing" in config.keys():
             self.population_growth_existing.load(config, self.config_file)
         
@@ -45,13 +37,8 @@ class Projection:
         if "economic_growth" in config.keys():
             self.economic_growth.load(config, self.config_file)
 
-        if "rainfall_increase" in config.keys():
-            self.precipitation_intensity.load(config, self.config_file)
-
-        if "storm_frequency_increase" in config.keys():
-            self.storminess.load(config, self.config_file)
-
-    def load(self) -> None:
+    def load(self, config_file: str = None) -> None:
+        self.config_file = config_file
         # Validate the existence of the configuration file
         if validate_existence_config_file(self.config_file):
             config = read_config(self.config_file)
