cb8cc22255476c0f94f351c75ff0bd6ede1345ef
tim-d-w <Tim.deWilde@Deltares.nl>
2023-02-22 16:57:09 +0100

    added floodwall measure and parent class hazard measure

---
+++ flood_adapt/object_model/hazard/measure/floodwall.py
@@ -0,0 +1,52 @@
+from flood_adapt.object_model.io.config_io import read_config
+from flood_adapt.object_model.validate.config import validate_existence_config_file, validate_content_config_file
+from flood_adapt.object_model.hazard.measure.hazard_measure import HazardMeasure
+
+class FloodWall(HazardMeasure):
+    """ Subclass of Measure describing the measure of elevating buildings by a specific height
+    """
+    def __init__(self, config_file: str = None) -> None:
+        super().__init__(config_file)
+
+    def set_default(self):
+        """ Sets the default values of the Elevate class attributes
+        """
+        super().set_default()
+        self.type = "floodwall"  # name reference that is used to know which class to use when the config file is read
+        self.elevation["value"] = None # elevation of flood wall
+        self.elevation["units"] = "m"  # the units that the height is given
+        self.elevation["type"] = "floodmap"  # type of height reference (can be "floodmap" or "datum")
+        self.polygon_file = None  # polygon file to use 
+        self.datum = None  # what is this for?
+        self.mandatory_keys.extend(["type","elevation", "polygon_file"])
+
+    def set_type(self, type: str)
+        self.type = type
+
+    def set_elevation(self, elevation: dict):
+        self.elevation["value"] = elevation["value"]
+        self.elevation["units"] = elevation["units"]
+        self.elevation["type"] = elevation["type"]
+
+    def set_polygon_file(self, polygon_file):
+        self.polygon_file = polygon_file
+
+    def set_datum(self, datum):
+        self.datum = datum
+
+    def load(self):
+        """ loads and updates the class attributes from a configuration file
+        """
+        super().load()
+        # Validate the existence of the configuration file
+        if validate_existence_config_file(self.config_file):
+            config = read_config(self.config_file)
+
+        # Validate that the mandatory keys are in the configuration file
+        if validate_content_config_file(config, self.config_file, self.mandatory_keys):
+            self.set_type(config["type"])
+            self.set_elevation(config["elevation"])
+            self.set_selection_type(config["selection_type"])
+            if config["datum"] is not None
+                self.set_datum(config["datum"])
+        return self
---
+++ flood_adapt/object_model/hazard/measure/hazard_measure.py
@@ -0,0 +1,39 @@
+from flood_adapt.object_model.io.config_io import read_config
+from flood_adapt.object_model.validate.config import validate_existence_config_file, validate_content_config_file
+from pathlib import Path
+
+class HazardMeasure:
+    """ Measure class that holds all the information for a specific measure type
+    """
+    def __init__(self, config_file: str = None) -> None:
+        self.set_default()
+        if config_file:
+            self.config_file = config_file
+
+    def set_default(self):
+        """ Sets the default values of the Measure class attributes
+        """
+        self.name = ""  # Name of the measure
+        self.long_name = ""  # Long name of the measure
+        self.config_file = None  # path to the configuration file connected with the measure
+        self.type = ""  # type of the measure
+        self.mandatory_keys = ["name", "long_name"]  # mandatory keys in the config file
+
+    def set_name(self, name: str):
+        self.name = name
+    
+    def set_long_name(self, long_name: str):
+        self.long_name = long_name
+
+    def load(self):
+        """ loads and updates the class attributes from a configuration file
+        """
+        # Validate the existence of the configuration file
+        if validate_existence_config_file(self.config_file):
+            config = read_config(self.config_file)
+
+        # Validate that the mandatory keys are in the configuration file
+        if validate_content_config_file(config, self.config_file, self.mandatory_keys):
+            self.set_name(config["name"])
+            self.set_long_name(config["long_name"])
+
