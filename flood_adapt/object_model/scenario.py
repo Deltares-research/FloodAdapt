0a32ac7745c744fdb64931e0181e4cd1242cc70a
Frederique <frederique.degroen@deltares.nl>
2023-02-22 14:40:46 +0100

    Added the Scenario and DirectImpact classes. The only issue I can find here are that now there is a direct_impact.py python file and a direct_impact folder with other files and classes. Don't know if this will become an issue.

---
+++ flood_adapt/object_model/scenario.py
@@ -0,0 +1,10 @@
+from flood_adapt.object_model.direct_impact import DirectImpact
+
+
+class Scenario:
+    """The Scenario class containing all information on a single scenario."""
+    def __init__(self):
+        self.set_default()
+
+    def set_default(self):
+        self.direct_impact = DirectImpact()
