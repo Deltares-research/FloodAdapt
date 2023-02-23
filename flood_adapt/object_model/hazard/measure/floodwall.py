eb9aa4f15ebcf2d5c61a485748e09004cc8ae882
tim-d-w <Tim.deWilde@Deltares.nl>
2023-02-23 11:40:22 +0100

    Restructed folders of hazard measure, updated import statements

--- flood_adapt/object_model/hazard/measure/floodwall.py
+++ flood_adapt/object_model/hazard/measure/floodwall.py
@@ -1,6 +1,6 @@
 from flood_adapt.object_model.io.config_io import read_config
 from flood_adapt.object_model.validate.config import validate_existence_config_file, validate_content_config_file
-from flood_adapt.object_model.hazard.measure_tdw.hazard_measure import HazardMeasure
+from flood_adapt.object_model.hazard.measure.hazard_measure import HazardMeasure
 
 class FloodWall(HazardMeasure):
     """ Subclass of Measure describing the measure of elevating buildings by a specific height
--- tests/test_hazard_measure.py
+++ tests/test_hazard_measure.py
@@ -3,7 +3,7 @@ from pathlib import Path
 test_database = Path().absolute() / 'tests' / 'test_database'
 
 def test_floodwall_measure_from_toml():
-    from flood_adapt.object_model.hazard.measure_tdw.floodwall import FloodWall
+    from flood_adapt.object_model.hazard.measure.floodwall import FloodWall
 
     test_toml = test_database / "charleston" / "input" / "measures" / "seawall" / "seawall.toml"
     assert test_toml.is_file()
