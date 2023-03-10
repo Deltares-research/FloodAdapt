296f30bdebf70c336ee5647bed0b28245b2c6be8
Tim de Wilde <120580434+tim-d-w@users.noreply.github.com>
2023-03-10 11:28:55 +0100

    Sfincs integrator 3 tdw (#62)
    * added sfincs_adapter base script
    
    * Updated siteconfig class with pydantic; updated tests for siteconfig; updated toml files; created unittest for discharge
    
    * updated siteconfig class with pydantic; updated tests for siteconfig; updated toml files; created unittest for discharge
    
    * sorted imports, added docstrings
    
    * used black for layout
    
    * updated wl bc; commented out remainder of functions
    
    * finished siteconfig, updated sfincs_adapter; used black for format
    
    * solved conflicts with main
    
    * comment out code that is not working at the moment; added docstrings
    
    * Update flood_adapt/object_model/site.py
    
    Co-authored-by: GundulaW <74292006+GundulaW@users.noreply.github.com>
    
    * Update flood_adapt/object_model/site.py
    
    Co-authored-by: GundulaW <74292006+GundulaW@users.noreply.github.com>
    
    * Update flood_adapt/object_model/site.py
    
    Co-authored-by: GundulaW <74292006+GundulaW@users.noreply.github.com>
    
    * updated review remarks
    
    * changed attrs name
    
    * format black
    
    ---------
    
    Co-authored-by: GundulaW <74292006+GundulaW@users.noreply.github.com>

---
+++ flood_adapt/integrator/sfincs_adapter.py
@@ -0,0 +1,63 @@
+from hydromt_sfincs import SfincsModel
+
+# from flood_adapt.object_model.validate.config import validate_existence_root_folder
+
+
+class SfincsAdapter:
+    def load_overland_sfincs_model(self, model_root: str = None):
+        """Loads overland sfincs model based on a root directory.
+
+        Args:
+            model_root (str, optional): Root directory of overland sfincs model. Defaults to None.
+        """
+        # Check if model root exists
+        # if validate_existence_root_folder(model_root):
+        #    self.model_root = model_root
+
+        self.sf_model = SfincsModel(root=model_root, mode="r+")
+        self.sf_model.read()
+
+    def add_wl_bc(self):
+        """Changes waterlevel of overland sfincs model based on new waterlevel time series."""
+
+        # Determine bnd points from reference overland model
+        gdf_locs = self.sf_model.forcing["bzs"].vector.to_gdf()
+        gdf_locs.crs = self.sf_model.crs
+
+        # Go from 1 timeseries to timeseries for all boundary points
+        df_ts = self.tide_ts
+        wl = df_ts[0]
+        for i in range(len(gdf_locs) - 1):
+            col_name = i + 1
+            df_ts[col_name] = wl
+
+        # HydroMT function: set waterlevel forcing from time series
+        self.sf_model.set_forcing_1d(
+            name="bzs", df_ts=df_ts, gdf_locs=gdf_locs, merge=False
+        )
+
+    # def run_sfincs_models(self):
+    #      pass
+
+    # def add_time(self, input_times: dict):
+    #      self.sf_model.config['tref'] = input_times['tref']
+    #      self.sf_model.config['tstart'] = input_times['tstart']
+    #      self.sf_model.config['tstop'] = input_times['tstop']
+
+    # def add_floodwall(self, polygon_file: str = None):
+
+    #     #HydroMT function: creates structure from dataframe
+    #     #Needs to be completed in hydromt_sfincs
+    #     self.sf_model.create_structures(structures_fn=polygon_file, stype='weir', overwrite=False)
+
+    # def add_meteo_bc(self,
+    #                  precip_ts: Union[xr.DataArray, pd.DataFrame, Dict[str, pd.DataFrame]] = None
+    #                  ):
+    #     #HydroMT function: set precipitation from times series
+    #     self.sf_model.set_forcing(name='precip',ts=precip_ts,xy=xy_precip)
+
+    # def add_discharge_bc(self,
+    #                      dis_ts: Union[xr.DataArray, pd.DataFrame, Dict[str, pd.DataFrame]] = None
+    #                      ):
+    #     #HydroMT function: set river forcing from times series
+    #     self.sf_model.set_forcing_1d(name='dis',ts=dis_ts,xy=self.sf_model.forcing['dis'].vector.to_gdf())
--- tests/test_object_model/test_site.py
+++ tests/test_object_model/test_site.py
@@ -1,25 +1,49 @@
 from pathlib import Path
 
+from flood_adapt.object_model.site import (
+    DemModel,
+    Obs_stationModel,
+    SfincsModel,
+    SiteConfig,
+)
+
 test_database = Path().absolute() / "tests" / "test_database"
 
 
 def test_read_site_toml():
-    from flood_adapt.object_model.site import SiteConfig
-
     test_toml = test_database / "charleston" / "static" / "site" / "charleston.toml"
+
     assert test_toml.is_file()
 
-    test_data = SiteConfig(config_path=test_toml)
+    test_data = SiteConfig.load_file(test_toml)
 
-    assert test_data
-    assert test_data.name == "charleston"
-    assert test_data.long_name == "Charleston, SC"
-    assert test_data.lat == 32.77
-    assert test_data.lon == -79.95
-    assert test_data.sfincs["cstype"] == "projected"
-    assert test_data.gui["tide_harmonic_amplitude"]["value"] == 3.0
-    assert test_data.dem["filename"] == "charleston_14m.tif"
-    assert test_data.fiat["aggregation_shapefiles"] == "subdivision.shp"
-    assert test_data.river["mean_discharge"]["units"] == "cfs"
-    assert test_data.obs_station["ID"] == 8665530
-    assert test_data.obs_station["mllw"]["value"] == 0.0
+    assert isinstance(test_data.model.name, str)
+    assert isinstance(test_data.model.sfincs, SfincsModel)
+    assert isinstance(test_data.model.dem, DemModel)
+    assert isinstance(test_data.model.obs_station, Obs_stationModel)
+    assert test_data.model.lat == 32.77
+    assert test_data.model.slr.vertical_offset.value == 0.6
+    assert test_data.model.fiat.exposure_crs == "EPSG:4326"
+    assert test_data.model.river.mean_discharge.value == 5000
+
+
+def test_read_site_toml_without_river():
+    test_toml = (
+        test_database
+        / "charleston"
+        / "static"
+        / "site"
+        / "charleston_without_river.toml"
+    )
+
+    assert test_toml.is_file()
+
+    test_data = SiteConfig.load_file(test_toml)
+
+    assert isinstance(test_data.model.name, str)
+    assert isinstance(test_data.model.sfincs, SfincsModel)
+    assert isinstance(test_data.model.dem, DemModel)
+    assert isinstance(test_data.model.obs_station, Obs_stationModel)
+    assert test_data.model.lat == 32.77
+    assert test_data.model.slr.vertical_offset.value == 0.6
+    assert test_data.model.fiat.exposure_crs == "EPSG:4326"
