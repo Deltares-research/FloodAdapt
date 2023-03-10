5164d25278128f40491e3dfee5976b7f28c6cfb2
tim-d-w <Tim.deWilde@Deltares.nl>
2023-03-10 10:49:34 +0100

    comment out code that is not working at the moment; added docstrings

--- flood_adapt/integrator/sfincs_adapter.py
+++ flood_adapt/integrator/sfincs_adapter.py
@@ -5,6 +5,11 @@ from hydromt_sfincs import SfincsModel
 
 class SfincsAdapter:
     def load_overland_sfincs_model(self, model_root: str = None):
+        """Loads overland sfincs model based on a root directory.
+
+        Args:
+            model_root (str, optional): Root directory of overland sfincs model. Defaults to None.
+        """
         # Check if model root exists
         # if validate_existence_root_folder(model_root):
         #    self.model_root = model_root
@@ -13,6 +18,7 @@ class SfincsAdapter:
         self.sf_model.read()
 
     def add_wl_bc(self):
+        """Changes waterlevel of overland sfincs model based on new waterlevel time series."""
 
         # Determine bnd points from reference overland model
         gdf_locs = self.sf_model.forcing["bzs"].vector.to_gdf()
@@ -30,31 +36,28 @@ class SfincsAdapter:
             name="bzs", df_ts=df_ts, gdf_locs=gdf_locs, merge=False
         )
 
+    # def run_sfincs_models(self):
+    #      pass
 
-""" Future functionality:  
-    def run_sfincs_models(self):
-         pass
+    # def add_time(self, input_times: dict):
+    #      self.sf_model.config['tref'] = input_times['tref']
+    #      self.sf_model.config['tstart'] = input_times['tstart']
+    #      self.sf_model.config['tstop'] = input_times['tstop']
 
-    def add_time(self, input_times: dict):
-         self.sf_model.config['tref'] = input_times['tref']
-         self.sf_model.config['tstart'] = input_times['tstart']
-         self.sf_model.config['tstop'] = input_times['tstop']
+    # def add_floodwall(self, polygon_file: str = None):
 
-    def add_floodwall(self, polygon_file: str = None):
-        
-        #HydroMT function: creates structure from dataframe
-        #Needs to be completed in hydromt_sfincs
-        self.sf_model.create_structures(structures_fn=polygon_file, stype='weir', overwrite=False) 
+    #     #HydroMT function: creates structure from dataframe
+    #     #Needs to be completed in hydromt_sfincs
+    #     self.sf_model.create_structures(structures_fn=polygon_file, stype='weir', overwrite=False)
 
-    def add_meteo_bc(self,
-                     precip_ts: Union[xr.DataArray, pd.DataFrame, Dict[str, pd.DataFrame]] = None
-                     ):
-        #HydroMT function: set precipitation from times series
-        self.sf_model.set_forcing(name='precip',ts=precip_ts,xy=xy_precip) 
+    # def add_meteo_bc(self,
+    #                  precip_ts: Union[xr.DataArray, pd.DataFrame, Dict[str, pd.DataFrame]] = None
+    #                  ):
+    #     #HydroMT function: set precipitation from times series
+    #     self.sf_model.set_forcing(name='precip',ts=precip_ts,xy=xy_precip)
 
-    def add_discharge_bc(self, 
-                         dis_ts: Union[xr.DataArray, pd.DataFrame, Dict[str, pd.DataFrame]] = None
-                         ):
-        #HydroMT function: set river forcing from times series
-        self.sf_model.set_forcing_1d(name='dis',ts=dis_ts,xy=self.sf_model.forcing['dis'].vector.to_gdf())
-"""
+    # def add_discharge_bc(self,
+    #                      dis_ts: Union[xr.DataArray, pd.DataFrame, Dict[str, pd.DataFrame]] = None
+    #                      ):
+    #     #HydroMT function: set river forcing from times series
+    #     self.sf_model.set_forcing_1d(name='dis',ts=dis_ts,xy=self.sf_model.forcing['dis'].vector.to_gdf())
