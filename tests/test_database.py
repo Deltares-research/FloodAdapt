6735b7540b01a9a5040217557d8fdfee30ca0727
Panos <panosatha90@gmail.com>
2023-03-14 08:22:40 +0100

    updated tests

---
+++ tests/test_database.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from flood_adapt.dbs_controller import Database
+
+test_database_path = Path().absolute() / "tests" / "test_database2"
+test_site_name = "Charleston"
+
+
+def test_database_controller():
+    Database(test_database_path, test_site_name)
