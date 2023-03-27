67de1b57d4ef6235d25a390d637bac7ab434dab3
GundulaW <74292006+GundulaW@users.noreply.github.com>
2023-03-27 12:13:18 +0200

    Create SFINCS model for synthetic event with tide, surge and discharge (#82)
    * made function run in hazard; updated sfincs adapter
    
    * creating SFINCS run from Hazard object, writing out changed water level timeseries crashing due to hydromt
    
    * debugging sfincs adapter
    
    * testing sfincs_adapter
    
    * Added timing and fixed writing waterlevel timeseries
    
    * adding wind, rain etc
    
    * add constant wind
    
    * generate wind in event.py
    
    * fixed generic shape function to work for river and rainfall as well
    
    * changed SFINCS version in site.toml
    
    * migrate test data with SFINCS base model to p-drive for speed
    
    * added constant and shape discharge to synthetic event
    
    * make black happy
    
    * Update sfincs_adapter.py
    
    * Update synthetic.py
    
    * revision after review by Tim
    
    * make black happy
    
    ---------
    
    Co-authored-by: tim-d-w <Tim.deWilde@Deltares.nl>
    Co-authored-by: Tim de Wilde <120580434+tim-d-w@users.noreply.github.com>

--- flood_adapt/integrator/sfincs_adapter.py
+++ flood_adapt/integrator/sfincs_adapter.py
@@ -1,12 +1,17 @@
+from datetime import datetime
+from pathlib import Path
 from typing import Optional
 
+import pandas as pd
 from hydromt_sfincs import SfincsModel
+
+from flood_adapt.object_model.hazard.event.event import EventModel
 
 # from flood_adapt.object_model.validate.config import validate_existence_root_folder
 
 
 class SfincsAdapter:
-    def load_overland_sfincs_model(self, model_root: Optional[str] = None):
+    def __init__(self, model_root: Optional[str] = None):
         """Loads overland sfincs model based on a root directory.
 
         Args:
@@ -19,32 +24,69 @@ class SfincsAdapter:
         self.sf_model = SfincsModel(root=model_root, mode="r+")
         self.sf_model.read()
 
-    def add_wl_bc(self):
+    def set_timing(self, event: EventModel):
         """Changes waterlevel of overland sfincs model based on new waterlevel time series."""
+
+        # Update timing of the model
+        # stop_time =
+        tstart = datetime.strptime(event.time.start_time, "%Y%m%d %H%M%S")
+        tstop = datetime.strptime(event.time.end_time, "%Y%m%d %H%M%S")
+        self.sf_model.set_config("tref", tstart)
+        self.sf_model.set_config("tstart", tstart)
+        self.sf_model.set_config("tstop", tstop)
+
+    def add_wl_bc(self, df_ts: pd.DataFrame):
+        """Changes waterlevel of overland sfincs model based on new waterlevel time series.
+
+        Parameters
+        ----------
+        df_ts : pd.DataFrame
+            time series of water level, index should be Pandas DateRange
+        """
 
         # Determine bnd points from reference overland model
         gdf_locs = self.sf_model.forcing["bzs"].vector.to_gdf()
         gdf_locs.crs = self.sf_model.crs
 
         # Go from 1 timeseries to timeseries for all boundary points
-        df_ts = self.tide_ts
-        wl = df_ts[0]
-        for i in range(len(gdf_locs) - 1):
-            col_name = i + 1
-            df_ts[col_name] = wl
+        for i in range(1, len(gdf_locs)):
+            df_ts[i + 1] = df_ts[1]
 
         # HydroMT function: set waterlevel forcing from time series
         self.sf_model.set_forcing_1d(
             name="bzs", df_ts=df_ts, gdf_locs=gdf_locs, merge=False
         )
 
+    def add_dis_bc(self, df_ts: pd.DataFrame):
+        """Changes discharge of overland sfincs model based on new discharge time series."""
+
+        # Determine bnd points from reference overland model
+        gdf_locs = self.sf_model.forcing["dis"].vector.to_gdf()
+        gdf_locs.crs = self.sf_model.crs
+
+        # Go from 1 timeseries to timeseries for all boundary points
+        for i in range(1, len(gdf_locs)):
+            df_ts[i] = df_ts[i]
+
+        # HydroMT function: set waterlevel forcing from time series
+        self.sf_model.set_forcing_1d(
+            name="dis", df_ts=df_ts, gdf_locs=gdf_locs, merge=False
+        )
+
+    def write_sfincs_model(self, path_out: Path):
+        """Write all the files for the sfincs model
+
+        Args:
+            path_out (Path): new root of sfincs model
+        """
+        # Change model root to new folder
+        self.sf_model.set_root(path_out, mode="w+")
+
+        # Write sfincs files in output folder
+        self.sf_model.write()
+
     # def run_sfincs_models(self):
     #      pass
-
-    # def add_time(self, input_times: dict):
-    #      self.sf_model.config['tref'] = input_times['tref']
-    #      self.sf_model.config['tstart'] = input_times['tstart']
-    #      self.sf_model.config['tstop'] = input_times['tstop']
 
     # def add_floodwall(self, polygon_file: str = None):
 
---
+++ tests/test_integrator/test_hazard_run.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+from flood_adapt.object_model.scenario import Scenario
+
+test_database = Path().absolute() / "tests" / "test_database"
+
+
+def test_hazard_run_synthetic_wl():
+    test_toml = (
+        test_database
+        / "charleston"
+        / "input"
+        / "scenarios"
+        / "current_extreme12ft_no_measures"
+        / "current_extreme12ft_no_measures.toml"
+    )
+
+    assert test_toml.is_file()
+
+    # use event template to get the associated Event child class
+    test_scenario = Scenario.load_file(test_toml)
+    test_scenario.init_object_model()
+    test_scenario.direct_impacts.hazard.run_sfincs(test_scenario.site_info)
+
+
+def test_hazard_run_synthetic_discharge():
+    test_toml = (
+        test_database
+        / "charleston"
+        / "input"
+        / "scenarios"
+        / "current_extreme12ft_rivershape_windconst_no_measures"
+        / "current_extreme12ft_rivershape_windconst_no_measures.toml"
+    )
+
+    assert test_toml.is_file()
+
+    # use event template to get the associated Event child class
+    test_scenario = Scenario.load_file(test_toml)
+    test_scenario.init_object_model()
+    test_scenario.direct_impacts.hazard.run_sfincs(test_scenario.site_info)
