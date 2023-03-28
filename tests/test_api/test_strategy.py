044cde5bb8b93c5749dba4feb3b39d5d6576d914
Panos <panosatha90@gmail.com>
2023-03-28 16:29:18 +0200

    pytest for strategies api

---
+++ tests/test_api/test_strategy.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+
+import pytest
+
+import flood_adapt.api.startup as api_startup
+import flood_adapt.api.strategies as api_strategies
+
+test_database_path = Path().absolute() / "tests" / "test_database_GUI"
+test_site_name = "charleston"
+
+
+def test_strategy_correct():
+    test_dict = {
+        "name": "strategy_comb",
+        "long_name": "strategy_comb",
+        "measures": [
+            "seawall",
+            "raise_property_aggregation_area",
+            "raise_property_polygon",
+            "raise_property_all_properties",
+        ],
+    }
+    # Initialize database object
+    database = api_startup.read_database(test_database_path, test_site_name)
+
+    # When user presses add strategy and chooses the measures
+    # the dictionary is returned and a Strategy object is created
+    with pytest.raises(ValueError):
+        # Assert error if there are overlapping measures
+        strategy = api_strategies.create_strategy(test_dict, database)
+
+    # correct measures
+    test_dict["measures"] = [
+        "seawall",
+        "raise_property_aggregation_area",
+        "raise_property_polygon",
+    ]
+    strategy = api_strategies.create_strategy(test_dict, database)
+
+    with pytest.raises(ValueError):
+        # Assert error if name already exists
+        api_strategies.save_strategy(strategy, database)
+
+    # Change name to something new
+    test_dict["name"] = "test1"
+    strategy = api_strategies.create_strategy(test_dict, database)
+    # If the name is not used before the measure is save in the database
+    api_strategies.save_strategy(strategy, database)
+    database.get_strategies()
+
+    # Try to delete a measure which is already used in a scenario
+    # with pytest.raises(ValueError):
+    #     api_strategies.delete_measure("", database)
+
+    # If user presses delete strategy the measure is deleted
+    api_strategies.delete_strategy("test1", database)
+    database.get_strategies()
