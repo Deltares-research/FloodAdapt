ab22c07a01579afe82f03ecbbeda0d38e0313721
Gundula Winter <gundulawinter@gmail.com>
2023-03-30 11:45:08 +0200

    make black happy

--- flood_adapt/api/scenarios.py
+++ flood_adapt/api/scenarios.py
@@ -8,12 +8,10 @@ def get_scenarios(database: IDatabase) -> dict[str, Any]:
     # sorting and filtering either with PyQt table or in the API
     return database.get_scenarios()
 
+
 def get_scenario(name: str, database: IDatabase) -> IScenario:
     return database.get_scenario(name)
 
+
 def delete_scenario(name: str, database: IDatabase) -> None:
     database.delete_scenario(name)
-
-
-
-
