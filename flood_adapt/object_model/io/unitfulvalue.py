aa6f30b71bd090d4de23f499922069623437d9d1
Gundula Winter <gundulawinter@gmail.com>
2023-04-07 14:59:08 +0200

    fixed double line in unitfulvalue

--- flood_adapt/object_model/io/unitfulvalue.py
+++ flood_adapt/object_model/io/unitfulvalue.py
@@ -7,7 +7,6 @@ class UnitTypesLength(str, Enum):
     meters = "meters"
     centimeters = "centimeters"
     millimeters = "millimeters"
-    millimeters = "millimeters"
     feet = "feet"
     inch = "inch"
 
