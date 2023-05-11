36e4977536f56e89884863673f05e598104e7c65
Panos Athanasiou <38324721+panosatha@users.noreply.github.com>
2023-05-11 11:38:51 +0200

    Hazard run checks and output api (#108)
    * initial commit
    
    * Updated get_objects_id method in ImpactMeasure class.
    The code was moved to hydromt-fiat, which is called now in the class
    to get the object ids.
    
    * Updated fiat template to much new version
    
    * updated tests and test data
    
    * corrected polygon implementation for
    get_object_ids method
    
    * updated api to use hydromt-fiat
    
    * cleaned up the test databases
    
    * udpates in object model
    
    * updated FIAT adapter
    
    * Update impact measure aggregation area selection
    to include aggr_area_type as an attribute
    
    * updated tests
    
    * tests updates
    
    * fiat adapter updates
    
    * added buyout and floodproof in object model
    
    * finilized inclusion of floodproof and buyout
    in the object model
    
    * updated tests
    
    * finilized fiat adapter scripts
    
    * update tests
    
    * small correction for ruff
    
    * now saves simulation path in Hazard object and makes a check if it has run during initialization.
    
    * added api functions for floodproofing and buyout
    
    * added hazard run test info in object model
    
    * expanded tests
    
    * -added run scenario method
    -added checks for using similar hazard runs when available.
    -added output api initial methods
    
    * updated tests
    
    * update transpose array from sfincs
    
    * small change in name in get methods
    
    * ruff correction
    
    * test corrections
    
    * added ruff to yml
    
    * small name changes in __eq__ method
    
    * fixed test
    
    * black correction
    
    * corrected run sfincs paths
    
    * black correction

--- flood_adapt/object_model/hazard/measure/hazard_measure.py
+++ flood_adapt/object_model/hazard/measure/hazard_measure.py
@@ -1,7 +1,12 @@
+import os
 from abc import ABC
+from typing import Union
+
+from flood_adapt.object_model.interface.measures import HazardMeasureModel
 
 
 class HazardMeasure(ABC):
     """HazardMeasure class that holds all the information for a specific measure type that affects the impact model"""
 
-    pass
+    attrs: HazardMeasureModel
+    database_input_path: Union[str, os.PathLike]
