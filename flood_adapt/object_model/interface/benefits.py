82b863d4efc12e406b9046ad30baf5b1ff9b225c
Panos <panosatha90@gmail.com>
2023-06-26 16:43:32 +0200

    Initial commit for the Benefits pydantic model class

---
+++ flood_adapt/object_model/interface/benefits.py
@@ -0,0 +1,40 @@
+import os
+from abc import ABC, abstractmethod
+from typing import Any, Optional, Union
+
+from pydantic import BaseModel
+
+
+class BenefitModel(BaseModel):
+    """BaseModel describing the expected variables and data types of a Benefit analysis object"""
+
+    name: str
+    description: Optional[str] = ""
+    event_set: str
+    strategy: str
+    projection: str
+    year: int
+    discount_rate: float
+    implementation_cost: Optional[float] = 0.0
+    annual_maint_cost: Optional[float] = 0.0
+
+
+class IBenefit(ABC):
+    attrs: BenefitModel
+
+    @staticmethod
+    @abstractmethod
+    def load_file(filepath: Union[str, os.PathLike]):
+        """get Benefit attributes from toml file"""
+        ...
+
+    @staticmethod
+    @abstractmethod
+    def load_dict(data: dict[str, Any], database_input_path: Union[str, os.PathLike]):
+        """get Benefit attributes from an object, e.g. when initialized from GUI"""
+        ...
+
+    @abstractmethod
+    def save(self, filepath: Union[str, os.PathLike]):
+        """save Benefit attributes to a toml file"""
+        ...
