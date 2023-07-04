69e9c753ecfa005cc8b1cfd33a0b7ecca22e369f
tim-d-w <tim.dewilde@deltares.nl>
2023-07-04 13:11:38 +0200

    altered functions from geojson to geodataframe

--- flood_adapt/object_model/hazard/measure/green_infrastructure.py
+++ flood_adapt/object_model/hazard/measure/green_infrastructure.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from typing import Any, Union
 
+import geopandas as gpd
+import pyproj
 import tomli
 import tomli_w
 
@@ -12,6 +14,7 @@ from flood_adapt.object_model.interface.measures import (
     GreenInfrastructureModel,
     IGreenInfrastructure,
 )
+from flood_adapt.object_model.interface.site import ISite
 
 
 class GreenInfrastructure(HazardMeasure, IGreenInfrastructure):
@@ -48,13 +51,56 @@ class GreenInfrastructure(HazardMeasure, IGreenInfrastructure):
         with open(filepath, "wb") as f:
             tomli_w.dump(self.attrs.dict(exclude_none=True), f)
 
-    def calculate_volume(self, area) -> float:
+    @staticmethod
+    def calculate_volume(
+        area: float, height: float = 0.0, percent_area: float = 100.0
+    ) -> float:
         """Determine volume from area of the polygon and infiltration height
+
+        Parameters
+        ----------
+        area : float
+            Area of polygon [m2] (calculated using calculate_polygon_area)
+        height : float, optional
+            Water height [m], by default 0.0
+        percent_area : float, optional
+            Percentage area covered by green infrastructure [%], by default 100.0
+
+        Returns
+        -------
+        float
+
 
         Returns
         -------
         float
             Volume [m3]
         """
-        volume = self.attrs.height * area
+        volume = area * height * percent_area / 100.0
         return volume
+
+    @staticmethod
+    def calculate_polygon_area(gdf: gpd.GeoDataFrame, site: ISite) -> float:
+        """Calculate area of a GeoDataFrame Polygon
+
+        Parameters
+        ----------
+        gdf : gpd.GeoDataFrame
+            Polygon object
+        site : ISite
+            site config (used for CRS)
+
+        Returns
+        -------
+        float
+            Area [m2]
+        """
+        # Determine local CRS
+        crs = pyproj.CRS.from_string(site.sfincs.csname)
+        gdf = gdf.to_crs(crs)
+
+        # The GeoJSON file contains only one polygon:
+        polygon = gdf.geometry.iloc[0]
+        # Calculate the area of the polygon
+        area = polygon.area
+        return area
