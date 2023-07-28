e4d7c30d9a7f7508eed278bd5b5243bc94cf5d90
Panos <panosatha90@gmail.com>
2023-07-28 16:19:15 +0300

    updated get_aggregation_areas to return dictionary of the geodataframes and get_measures methods to return geometries for aggregation areas

--- flood_adapt/dbs_controller.py
+++ flood_adapt/dbs_controller.py
@@ -59,7 +59,7 @@ class Database(IDatabase):
         # self.update()
 
     # General methods
-    def get_aggregation_areas(self) -> list[GeoDataFrame]:
+    def get_aggregation_areas(self) -> dict:
         """Get a list of the aggregation areas that are provided in the site configuration.
         These are expected to much the ones in the FIAT model
 
@@ -68,19 +68,16 @@ class Database(IDatabase):
         list[GeoDataFrame]
             list of geodataframes with the polygons defining the aggregation areas
         """
-        aggregation_areas = [
-            gpd.read_file(
-                self.input_path.parent / "static" / "site" / aggr_dict.file
-            ).to_crs(4326)
-            for aggr_dict in self.site.attrs.fiat.aggregation
-        ]
-        # Make sure they are ordered alphabetically
-        aggregation_areas = [
-            aggregation_areas.sort_values(
-                by=self.site.attrs.fiat.aggregation[i].field_name
+        aggregation_areas = {}
+        for aggr_dict in self.site.attrs.fiat.aggregation:
+            aggregation_areas[aggr_dict.name] = gpd.read_file(self.input_path.parent / "static" / "site" / aggr_dict.file).to_crs(4326)
+            # Use always the same column name for name labels
+            aggregation_areas[aggr_dict.name].rename(columns={aggr_dict.field_name: "name"}, inplace=True)
+            # Make sure they are ordered alphabetically
+            aggregation_areas[aggr_dict.name].sort_values(
+                by="name"
             ).reset_index(drop=True)
-            for i, aggregation_areas in enumerate(aggregation_areas)
-        ]
+
         return aggregation_areas
 
     def get_slr_scn_names(self) -> list:
@@ -968,12 +965,23 @@ class Database(IDatabase):
         objects = [MeasureFactory.get_measure_object(path) for path in measures["path"]]
         measures["name"] = [obj.attrs.name for obj in objects]
         measures["long_name"] = [obj.attrs.long_name for obj in objects]
-        measures["geometry"] = [
-            gpd.read_file(path.parent.joinpath(obj.attrs.polygon_file))
-            if obj.attrs.polygon_file is not None
-            else None
-            for (path, obj) in zip(measures["path"], objects)
-        ]
+        
+        geometries = []
+        for (path, obj) in zip(measures["path"], objects):
+            # If polygon is used read the polygon file
+            if obj.attrs.polygon_file is not None:
+                geometries.append(gpd.read_file(path.parent.joinpath(obj.attrs.polygon_file)))
+            # If aggregation area is used read the polygon from the aggregation area name
+            elif obj.attrs.aggregation_area_name is not None:
+                # TODO Maybe it makes more sense to have the geometry returned here?
+                aggr_areas = self.get_aggregation_areas()
+                gdf = aggr_areas[obj.attrs.aggregation_area_type]
+                geometries.append(gdf.loc[gdf["name"]==obj.attrs.aggregation_area_name, :])
+            # Else assign a None value
+            else:
+                geometries.append(None)
+
+        measures["geometry"] = geometries
         return measures
 
     def get_strategies(self) -> dict[str, Any]:
--- flood_adapt/object_model/interface/database.py
+++ flood_adapt/object_model/interface/database.py
@@ -23,7 +23,7 @@ class IDatabase(ABC):
         ...
 
     @abstractmethod
-    def get_aggregation_areas(self) -> list[GeoDataFrame]:
+    def get_aggregation_areas(self) -> dict:
         ...
 
     @abstractmethod
