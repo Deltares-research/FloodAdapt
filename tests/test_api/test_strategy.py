40904aeb10e93a65193521225393fcf9c687114a
GundulaW <gundulawinter@gmail.com>
2023-08-15 12:23:45 +0200

    changing "test1" to sth more descriptive

--- tests/test_api/test_strategy.py
+++ tests/test_api/test_strategy.py
@@ -43,7 +43,7 @@ def test_strategy():
         api_strategies.save_strategy(strategy, database)
 
     # Change name to something new
-    test_dict["name"] = "test1"
+    test_dict["name"] = "test_strat_1"
     # delete an old one if it already exists
     if database.input_path.joinpath("strategies", test_dict["name"]).is_dir():
         shutil.rmtree(database.input_path.joinpath("strategies", test_dict["name"]))
@@ -59,5 +59,5 @@ def test_strategy():
     #     api_strategies.delete_measure("", database)
 
     # If user presses delete strategy the measure is deleted
-    api_strategies.delete_strategy("test1", database)
+    api_strategies.delete_strategy("test_strat_1", database)
     database.get_strategies()
