e327f519bffa81a0bd71dfe176b1c1b380880599
Panos Athanasiou <Panos.Athanasiou@deltares.nl>
2023-10-31 19:14:36 +0100

    benefit tests updates to use dummy data

--- tests/test_object_model/test_benefits.py
+++ tests/test_object_model/test_benefits.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import pandas as pd
 import pytest
+import tomli
 
 from flood_adapt.dbs_controller import Database
 from flood_adapt.object_model.benefit import Benefit
@@ -41,47 +42,6 @@ def test_check_scenarios(cleanup_database):
     assert isinstance(df_check, pd.DataFrame)
 
 
-@pytest.mark.skip(reason="Takes too long")
-def test_run_CBA(cleanup_database):
-    dbs = Database(test_database, "charleston")
-
-    benefit_toml = (
-        test_database
-        / "charleston"
-        / "input"
-        / "benefits"
-        / "benefit_raise_properties_2050"
-        / "benefit_raise_properties_2050.toml"
-    )
-
-    assert benefit_toml.is_file()
-
-    benefit = Benefit.load_file(benefit_toml)
-
-    # Create missing scenarios
-    dbs.create_benefit_scenarios(benefit)
-
-    # Check that error is returned if not all runs are finished
-    if not all(benefit.scenarios["scenario run"]):
-        with pytest.raises(RuntimeError):
-            # Assert error if not yet run
-            benefit.run_cost_benefit()
-
-    # Runs missing scenarios
-    for name, row in benefit.scenarios.iterrows():
-        if not row["scenario run"]:
-            dbs.run_scenario(row["scenario created"])
-
-    # Check which
-    benefit.check_scenarios()
-
-    # Test if all scenarios are ^run^
-    assert all(benefit.scenarios["scenario run"])
-
-    benefit.run_cost_benefit()
-
-
-@pytest.mark.skip(reason="Takes too long")
 def test_run_benefit_analysis(cleanup_database):
     dbs = Database(test_database, "charleston")
 
@@ -107,15 +67,117 @@ def test_run_benefit_analysis(cleanup_database):
             # Assert error if not yet run
             benefit.run_cost_benefit()
 
-    # Runs missing scenarios
+    # Create dummy results to use for benefit analysis
+    damages_dummy = {"current_no_measures": 100e6,
+                     "current_with_strategy": 50e6,
+                     "future_no_measures": 300e6,
+                     "future_with_strategy": 180e6}
+    
     for name, row in benefit.scenarios.iterrows():
-        if not row["scenario run"]:
-            dbs.run_scenario(row["scenario created"])
+        # Create output folder
+        output_path = test_database / "charleston" / "output" / "Scenarios" / row["scenario created"]
+        if not output_path.exists():
+            output_path.mkdir(parents=True)
+        # Create dummy metrics file
+        dummy_metrics = pd.DataFrame({"Description": "", 
+                                      "Show In Metrics Table": "TRUE", 
+                                      "Long Name": "", 
+                                      "Value": damages_dummy[name]
+                                      }, 
+                                      index=["ExpectedAnnualDamages"])
+        dummy_metrics.to_csv(output_path.joinpath(f"Infometrics_{row['scenario created']}.csv"))
 
-    # Check which
-    benefit.check_scenarios()
+    # Run benefit analysis with dummy data
+    benefit.cba()
 
-    # Test if all scenarios are ^run^
-    assert all(benefit.scenarios["scenario run"])
+    # Read results
+    results_path = test_database / "charleston" / "output" / "Benefits" / benefit.attrs.name 
+    with open(results_path.joinpath("results.toml"), mode="rb") as fp:
+        results = tomli.load(fp)
 
-    benefit.run_cost_benefit()
+    # get results
+    tot_benefits = results["benefits"]
+    # get time-series
+    csv_results = pd.read_csv(results_path.joinpath("time_series.csv"))
+    tot_benefits2 = csv_results["benefits_discounted"].sum()
+
+    # assert if time-series and totals are consistent
+    assert tot_benefits == tot_benefits2
+
+    # assert if results are equal to the expected values based on the input
+    assert tot_benefits == 963433925
+
+
+def test_run_CBA(cleanup_database):
+    dbs = Database(test_database, "charleston")
+
+    benefit_toml = (
+        test_database
+        / "charleston"
+        / "input"
+        / "benefits"
+        / "benefit_raise_properties_2050"
+        / "benefit_raise_properties_2050.toml"
+    )
+
+    assert benefit_toml.is_file()
+
+    benefit = Benefit.load_file(benefit_toml)
+
+    # Create missing scenarios
+    dbs.create_benefit_scenarios(benefit)
+
+    # Check that error is returned if not all runs are finished
+    if not all(benefit.scenarios["scenario run"]):
+        with pytest.raises(RuntimeError):
+            # Assert error if not yet run
+            benefit.run_cost_benefit()
+
+    # Create dummy results to use for benefit analysis
+    damages_dummy = {"current_no_measures": 100e6,
+                     "current_with_strategy": 50e6,
+                     "future_no_measures": 300e6,
+                     "future_with_strategy": 180e6}
+    
+    for name, row in benefit.scenarios.iterrows():
+        # Create output folder
+        output_path = test_database / "charleston" / "output" / "Scenarios" / row["scenario created"]
+        if not output_path.exists():
+            output_path.mkdir(parents=True)
+        # Create dummy metrics file
+        dummy_metrics = pd.DataFrame({"Description": "", 
+                                      "Show In Metrics Table": "TRUE", 
+                                      "Long Name": "", 
+                                      "Value": damages_dummy[name]
+                                      }, 
+                                      index=["ExpectedAnnualDamages"])
+        dummy_metrics.to_csv(output_path.joinpath(f"Infometrics_{row['scenario created']}.csv"))
+
+    # Run benefit analysis with dummy data
+    benefit.cba()
+
+    # Read results
+    results_path = test_database / "charleston" / "output" / "Benefits" / benefit.attrs.name 
+    with open(results_path.joinpath("results.toml"), mode="rb") as fp:
+        results = tomli.load(fp)
+
+    # get results
+    tot_benefits = results["benefits"]
+    tot_costs = results["costs"]
+    # get time-series
+    csv_results = pd.read_csv(results_path.joinpath("time_series.csv"))
+    tot_benefits2 = csv_results["benefits_discounted"].sum()
+    tot_costs2 = csv_results["costs_discounted"].sum()
+
+    # assert if time-series and totals are consistent
+    assert tot_benefits == tot_benefits2
+    assert tot_costs == tot_costs2
+
+    # assert if results are equal to the expected values based on the input
+    assert tot_benefits == 963433925
+    assert tot_costs == 201198671
+    assert results["BCR"]== 4.79
+    assert results["NPV"]== 762235253
+    assert results["IRR"]== 0.394
+
+
