332dd6423c0e463e9e7e5edd153fde9ae5908664
Daley Adrichem <Daley.Adrichem@deltares.nl>
2023-11-17 13:17:41 +0100

    Test

--- flood_adapt/integrator/sfincs_adapter.py
+++ flood_adapt/integrator/sfincs_adapter.py
@@ -459,7 +459,7 @@ class SfincsAdapter:
             zsmax=floodmap_conversion * zsmax,
             dep=floodmap_conversion * dem,
             hmin=0.01,
-            floodmap_fn=floodmap_fn,
+            floodmap_fn=str(floodmap_fn),
         )
         print( "Test3")
 
