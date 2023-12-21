0dab01492056ab945128fdbf3f83a4d832a7f9f5
Panos Athanasiou <Panos.Athanasiou@deltares.nl>
2023-12-21 21:31:42 +0100

    black fix

--- tests/test_object_model/test_benefits.py
+++ tests/test_object_model/test_benefits.py
@@ -11,6 +11,7 @@ from flood_adapt.object_model.benefit import Benefit
 test_database = Path().absolute() / "tests" / "test_database"
 rng = np.random.default_rng(2021)
 
+
 def test_benefit_read(cleanup_database):
     benefit_toml = (
         test_database
