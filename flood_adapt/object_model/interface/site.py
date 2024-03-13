4459654ddfd96c19ffe6303e02c5125fd8f2cbbd
Panos Athanasiou <Panos.Athanasiou@deltares.nl>
2024-03-13 11:16:07 +0100

    changes optional variables in site.toml

--- flood_adapt/object_model/interface/site.py
+++ flood_adapt/object_model/interface/site.py
@@ -80,14 +80,14 @@ class DamageType(str, Enum):
 class MapboxLayersModel(BaseModel):
     """class describing the configuration of the mapbox layers in the gui"""
 
-    buildings_min_zoom_level: Optional[int] = 13
+    buildings_min_zoom_level: int = 13
     flood_map_depth_min: float
     flood_map_zbmax: float
     flood_map_bins: list[float]
     flood_map_colors: list[str]
     aggregation_dmg_bins: list[float]
     aggregation_dmg_colors: list[str]
-    footprints_dmg_type: Optional[DamageType] = "absolute"
+    footprints_dmg_type: DamageType = "absolute"
     footprints_dmg_bins: list[float]
     footprints_dmg_colors: list[str]
     svi_bins: Optional[list[float]] = []
