2aea40c9797b7ebff9226b5188e98ef216f34c9d
Panos Athanasiou <Panos.Athanasiou@deltares.nl>
2024-06-06 14:05:14 +0200

    small correction in import

--- flood_adapt/dbs_controller.py
+++ flood_adapt/dbs_controller.py
@@ -15,9 +15,6 @@ from cht_cyclones.tropical_cyclone import TropicalCyclone
 from geopandas import GeoDataFrame
 from hydromt_sfincs.quadtree import QuadtreeGrid
 
-from build.lib.flood_adapt.integrator.interface.direct_impacts_adapter import (
-    DirectImpactsAdapter,
-)
 from flood_adapt.dbs_classes.dbs_benefit import DbsBenefit
 from flood_adapt.dbs_classes.dbs_event import DbsEvent
 from flood_adapt.dbs_classes.dbs_measure import DbsMeasure
@@ -25,6 +22,9 @@ from flood_adapt.dbs_classes.dbs_projection import DbsProjection
 from flood_adapt.dbs_classes.dbs_scenario import DbsScenario
 from flood_adapt.dbs_classes.dbs_static import DbsStatic
 from flood_adapt.dbs_classes.dbs_strategy import DbsStrategy
+from flood_adapt.integrator.interface.direct_impacts_adapter import (
+    DirectImpactsAdapter,
+)
 from flood_adapt.integrator.interface.direct_impacts_adapter_factory import (
     DirectImpactsAdapterFactory,
 )
