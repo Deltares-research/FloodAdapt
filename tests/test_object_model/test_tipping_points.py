fa09dbf09b2537e6507b8452a460f071ad2914d9
Henrique Goulart <45360568+dumontgoulart@users.noreply.github.com>
2024-07-09 17:03:02 +0200

    refactor functions

--- tests/test_object_model/test_tipping_points.py
+++ tests/test_object_model/test_tipping_points.py
@@ -11,7 +11,7 @@ from flood_adapt.object_model.interface.tipping_points import ITipPoint
 
 
 @pytest.fixture
-def setup_database():
+def test_db():
     # Mock the database setup or configure a test database
     read_database(
         rf"C:\\Users\\morenodu\\OneDrive - Stichting Deltares\\Documents\\GitHub\\FloodAdapt-Database",
@@ -22,7 +22,8 @@ def setup_database():
     )
 
 
-def test_tipping_point_creation(setup_database):
+@pytest.fixture()
+def tp_dict():
     # Setup
     tp_dict = {
         "name": "tipping_point_test",
@@ -36,6 +37,11 @@ def test_tipping_point_creation(setup_database):
             ("FullyFloodedRoads", 2000, "greater"),
         ],
     }
+    return tp_dict
+
+
+def test_createTippingPoints_scenariosAlreadyExist_notDuplicated(test_db, tp_dict):
+
     # Exercise
     test_point = TippingPoint.load_dict(tp_dict)
     test_point.create_tp_scenarios()
@@ -45,23 +51,14 @@ def test_tipping_point_creation(setup_database):
     assert isinstance(test_point, TippingPoint)
 
 
-def test_run_scenarios(setup_database):
-    tp_dict = {
-        "name": "tipping_point_test",
-        "description": "",
-        "event_set": "extreme12ft",
-        "strategy": "no_measures",
-        "projection": "current",
-        "sealevelrise": [0.5, 1.0, 1.5],
-        "tipping_point_metric": [
-            ("FloodedAll", 34195.0, "greater"),
-            ("FullyFloodedRoads", 2000, "greater"),
-        ],
-    }
+def test_run_scenarios(test_db, tp_dict):
+
     test_point = TippingPoint.load_dict(tp_dict)
     test_point.create_tp_scenarios()
     # This should run without raising an exception
     test_point.run_tp_scenarios()
+
+    assert test_point is not None
 
 
 # database = read_database(
