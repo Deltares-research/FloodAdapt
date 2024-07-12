8e8d841bca686cff90d42d703bd717f945ed2f30
Daley Adrichem <Daley.Adrichem@deltares.nl>
2024-07-12 12:23:27 +0200

    Make test copy stuff for debugging

--- tests/test_api/test_output.py
+++ tests/test_api/test_output.py
@@ -1,6 +1,9 @@
+import shutil
+
 import geopandas as gpd
 import pandas as pd
 import pytest
+from ..conftest import database_path, database_root
 
 import flood_adapt.api.output as api_output
 import flood_adapt.api.scenarios as api_scenarios
@@ -15,6 +18,9 @@ class TestAPI_Output:
 
     def test_impact_metrics(self, scenario):
         metrics = api_output.get_infometrics(scenario)
+        source_folder = f"{database_path}\\output\\Scenarios\\{scenario}\\"
+        destination_folder = f"{database_root}\\temp\\"
+        shutil.copytree(source_folder, destination_folder)
         assert isinstance(metrics, pd.DataFrame)
 
     def test_impact_footprints(self, scenario):
