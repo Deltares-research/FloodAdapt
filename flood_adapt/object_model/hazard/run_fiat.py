97396dfc3f7192d08427f2f38e96c5d5bece659d
Luuk Blom <Luuk.Blom@deltares.nl>
2024-10-17 13:45:41 +0200

    remove unused Docker code fix bug in fiat path

--- flood_adapt/object_model/hazard/run_fiat.py
+++ flood_adapt/object_model/hazard/run_fiat.py
@@ -1,4 +1,3 @@
-import os
 import subprocess
 import sys
 from pathlib import Path
@@ -12,6 +11,10 @@ logger = FloodAdaptLogging.getLogger("FiatAdapter")
 
 def execute_fiat(simulation_dir: Path) -> bool:
     logger.info(f"Running FIAT model for {'-'.join(simulation_dir.parts[-2:])}.")
+    logger.debug(
+        f"FIAT path: {Settings().fiat_path}, simulation directory: {simulation_dir}"
+    )
+
     if sys.platform == "win32":
         run_success = _run_fiat_win(simulation_dir)
     elif sys.platform == "linux":
@@ -31,7 +34,7 @@ def _run_fiat_win(simulation_dir: Path) -> bool:
                 f'"{Settings().fiat_path.as_posix()}" run settings.toml',
                 stdout=log_handler,
                 stderr=log_handler,
-                env=os.environ.copy(),  # need environment variables from runtime hooks
+                # env=os.environ.copy(),  # need environment variables from runtime hooks
                 check=True,
                 shell=True,
             )
@@ -47,7 +50,7 @@ def _run_fiat_linux(simulation_dir: Path) -> bool:
     simulation_dir : Path
         The directory containing all the input files for the FIAT model.
         Path to the simulation directory to be mounted in the Docker container.
-        
+
     Returns
     -------
     bool
@@ -68,9 +71,9 @@ def _run_fiat_linux(simulation_dir: Path) -> bool:
         "-v",
         f"{simulation_dir}:/home/deltares/data",  # Mount the input directory to /home/deltares
         "fiat",  # Docker image
-        "fiat run /home/deltares/data/settings.toml", # Run command
+        "fiat run /home/deltares/data/settings.toml",  # Run command
     ]
-    
+
     log_file = simulation_dir / "fiat.log"
 
     with open(log_file, "w") as log_handler:
--- tests/test_integrator/test_run_fiat.py
+++ tests/test_integrator/test_run_fiat.py
@@ -1,28 +1,33 @@
-from pathlib import Path
 import shutil
-from unittest.mock import patch
-
-from flood_adapt.object_model.hazard.run_fiat import execute_fiat
 
 import pytest
 
+from flood_adapt.log import FloodAdaptLogging
+from flood_adapt.object_model.hazard.run_fiat import execute_fiat
+
+logger = FloodAdaptLogging.getLogger("test_run_fiat")
+
+
 @pytest.fixture
-def setup_fiat_dir(test_db, tmp_path):
-    scn = test_db.scenarios.get("hazard_already_run")
+def setup_scn_dir(test_db, tmp_path):
+    scn = test_db.scenarios.get("all_projections_extreme12ft_strategy_comb")
     if not scn.direct_impacts.hazard.has_run:
         scn.direct_impacts.hazard.preprocess_models()
         scn.direct_impacts.hazard.run_models()
         scn.direct_impacts.hazard.postprocess_models()
     else:
         print(f"Hazard for scenario '{scn.attrs.name}' has already been run.")
-    
+
     scn.direct_impacts.preprocess_models()
     shutil.copytree(scn.results_path, tmp_path / "test_fiat")
     return tmp_path / "test_fiat"
 
-def test_run_fiat_on_linux(setup_fiat_dir):   
+
+def test_run_fiat_on_linux(setup_scn_dir):
+    logger.info(f"Running test_run_fiat_on_linux for {setup_scn_dir}")
+
     # Act
-    success = execute_fiat(setup_fiat_dir)
+    success = execute_fiat(setup_scn_dir / "Impacts" / "fiat_model")
 
     # Assert
     assert success
