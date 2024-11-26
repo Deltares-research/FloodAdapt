0d861ecfcffee0956613656ae81d7a07e1f2967e
Sam Vente <savente93@proton.me>
2024-11-26 13:39:30 +0100

    chore(object_model): Rename objects for consistency (#588)
    * chore(objects): rename some objects for more consistent naming
    
    * chore(site): update default frequency flood model
    
    * chore: remove duplicate enum
    
    * chore: linting
    
    * fix typo
    
    * fix: fix import problem
    
    * chore: linting

--- flood_adapt/object_model/interface/events.py
+++ flood_adapt/object_model/interface/events.py
@@ -141,7 +141,7 @@ class SurgeModel(BaseModel):
     """BaseModel describing the expected variables and data types for harmonic tide parameters of synthetic model."""
 
     source: SurgeSource
-    shape_type: Optional[str] = "gaussian"
+    shape_type: ShapeType = ShapeType.gaussian
     shape_duration: Optional[float] = None
     shape_peak_time: Optional[float] = None
     shape_peak: Optional[UnitfulLength] = None
@@ -162,7 +162,7 @@ class EventModel(BaseModel):  # add WindModel etc as this is shared among all? t
     """BaseModel describing the expected variables and data types of attributes common to all event types."""
 
     name: str = Field(..., min_length=1, pattern='^[^<>:"/\\\\|?* ]*$')
-    description: Optional[str] = ""
+    description: str = ""
     mode: Mode
     template: Template
     timing: Timing
