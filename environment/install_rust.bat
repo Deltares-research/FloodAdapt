e8539f12457ea6cd6b10b5b949b0e6bc0624ae2d
Luuk Blom <Luuk.Blom@deltares.nl>
2024-12-09 15:02:42 +0100

    debug commit for teamcity

--- environment/install_rust.bat
+++ environment/install_rust.bat
@@ -5,6 +5,9 @@
 ::
 :: Currently, this script is required due to hydromt-sfincs requiring it, but not installing it.
 
+:: The default installation directory on Windows
+SET INSTALL_DIR="%USERPROFILE%\.cargo\bin"
+
 :: Check if rust and cargo are available as is
 cargo --version >nul 2>&1 && rustc --version >nul 2>&1
 IF %ERRORLEVEL% EQU 0 (
@@ -12,7 +15,7 @@ IF %ERRORLEVEL% EQU 0 (
     exit /b
 )
 
-IF EXIST "%USERPROFILE%\.cargo\bin" (
+IF EXIST "%INSTALL_DIR%" (
     :: Add to PATH if install dir does exists
     echo Rust installation dir exists, but could not find rust in PATH, skipping re-install. Please check your PATH.
     echo "%PATH%"
@@ -30,13 +33,18 @@ cargo --version >nul 2>&1 && rustc --version >nul 2>&1
 IF %ERRORLEVEL% EQU 0 (
     echo Rust and cargo already in the PATH.
 ) ELSE (
-    setx PATH "%USERPROFILE%\.cargo\bin;%PATH%"
-    set PATH="%USERPROFILE%\.cargo\bin;%PATH%"
-
-    echo Added Rust to PATH...
-    echo "%PATH%"
+    echo Add Rust to PATH for this session...
+    SET "PATH=%INSTALL_DIR%;%PATH%"
 
     :: Verify
     cargo --version
     rustc --version
+
+    :: Display instructions
+    echo.
+    echo If you want to add it to your path permanently, you can do so by running the following command in your terminal:
+    echo.
+    echo SETX PATH "%%INSTALL_DIR%%;%%PATH%%"
+    echo.
+    echo Where %%INSTALL_DIR%% is the path to the cargo bin directory. default: %%USERPROFILE%%\.cargo\bin, which expands to %INSTALL_DIR%
 )
