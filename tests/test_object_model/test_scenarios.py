454c52b9bfd09665be96b412d1043e0042d58ce9
LuukBlom <153174893+LuukBlom@users.noreply.github.com>
2025-01-08 15:44:50 +0100

    test(misc): remove obsolete tests (#625)
    During the refactor I added a test file, but forgot to remove it

--- tests/test_object_model/test_scenarios.py
+++ tests/test_object_model/test_scenarios.py
@@ -14,30 +14,16 @@ from flood_adapt.object_model.scenario import Scenario
 
 
 @pytest.fixture(autouse=True)
-def test_tomls(test_db) -> list:
-    toml_files = [
-        test_db.input_path
-        / "scenarios"
-        / "all_projections_extreme12ft_strategy_comb"
-        / "all_projections_extreme12ft_strategy_comb.toml",
-        test_db.input_path
-        / "scenarios"
-        / "current_extreme12ft_no_measures"
-        / "current_extreme12ft_no_measures.toml",
+def test_scenarios(test_db):
+    test_scns = [
+        "current_extreme12ft_no_measures",
+        "all_projections_extreme12ft_strategy_comb",
     ]
-    yield toml_files
+    yield test_scns
 
 
-@pytest.fixture(autouse=True)
-def test_scenarios(test_db, test_tomls) -> dict[str, Scenario]:
-    test_scenarios = {
-        toml_file.name: Scenario.load_file(toml_file) for toml_file in test_tomls
-    }
-    yield test_scenarios
-
-
-def test_init_valid_input(test_db, test_scenarios):
-    test_scenario = test_scenarios["all_projections_extreme12ft_strategy_comb.toml"]
+def test_init_valid_input(test_db, test_scenarios: dict[str, Scenario]):
+    test_scenario = test_db.scenarios.get("all_projections_extreme12ft_strategy_comb")
 
     assert isinstance(test_scenario.site_info, Site)
     assert isinstance(test_scenario.direct_impacts, DirectImpacts)
@@ -46,6 +32,7 @@ def test_init_valid_input(test_db, test_scenarios):
     )
     assert isinstance(test_scenario.direct_impacts.impact_strategy, ImpactStrategy)
     assert isinstance(test_scenario.direct_impacts.hazard, FloodMap)
+
     assert isinstance(
         test_scenario.direct_impacts.hazard.hazard_strategy, HazardStrategy
     )
@@ -57,24 +44,43 @@ def test_init_valid_input(test_db, test_scenarios):
 class Test_scenario_run:
     @pytest.fixture(scope="class")
     def test_scenario_before_after_run(self, test_db_class: IDatabase):
-        before_run_name = "current_extreme12ft_no_measures"
-        after_run_name = "current_extreme12ft_no_measures_run"
+        run_name = "all_projections_extreme12ft_strategy_comb"
+        not_run_name = f"{run_name}_NOT_RUN"
 
         test_db_class.scenarios.copy(
-            old_name=before_run_name,
-            new_name=after_run_name,
+            old_name=run_name,
+            new_name=not_run_name,
             new_description="temp_description",
         )
 
-        after_run = test_db_class.scenarios.get(after_run_name)
-        after_run.run()
+        to_run = test_db_class.scenarios.get(run_name)
+        to_run.run()
 
-        yield test_db_class, before_run_name, after_run_name
+        yield test_db_class, run_name, not_run_name
 
-    def test_run_change_has_run(self, test_scenario_before_after_run):
-        test_db, before_run, after_run = test_scenario_before_after_run
-        before_run = test_db.scenarios.get(before_run)
-        after_run = test_db.scenarios.get(after_run)
+    def test_run_change_has_run(
+        self, test_scenario_before_after_run: tuple[IDatabase, str, str]
+    ):
+        test_db, run_name, not_run_name = test_scenario_before_after_run
 
-        assert not before_run.direct_impacts.hazard.has_run
-        assert after_run.direct_impacts.hazard.has_run
+        not_run = test_db.scenarios.get(not_run_name)
+        run = test_db.scenarios.get(run_name)
+
+        assert not not_run.direct_impacts.hazard.has_run
+        assert run.direct_impacts.hazard.has_run
+
+
+@pytest.mark.parametrize(
+    "scn_name",
+    [
+        "all_projections_extreme12ft_strategy_comb",
+        "current_extreme12ft_no_measures",
+        "current_extreme12ft_raise_datum",
+        "current_extreme12ft_rivershape_windconst_no_measures",
+        "current_extreme12ft_strategy_impact_comb",
+    ],
+)
+def test_run_on_all_scn(test_db, scn_name):
+    scn = test_db.scenarios.get(scn_name)
+    scn.run()
+    assert scn.direct_impacts.hazard.has_run
