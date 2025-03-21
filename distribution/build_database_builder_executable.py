a775e72def576bb47182a58282b572603342c98b
LuukBlom <153174893+LuukBlom@users.noreply.github.com>
2025-03-21 09:58:50 +0100

    feat(distribution): update database builder executable creation script (#723)
    copy_shapely function and update dependencies list

--- distribution/build_database_builder_executable.py
+++ distribution/build_database_builder_executable.py
@@ -10,7 +10,6 @@ except Exception:
     subprocess.check_call([sys.executable, "-m", "pip", "install", "pyinstaller==6.7"])
     import PyInstaller.__main__  # noQA
 
-from PyInstaller.utils.hooks import collect_delvewheel_libs_directory  # noQA
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
 DIST_ROOT = Path(__file__).resolve().parents[0]
@@ -20,25 +19,35 @@ DIST_DIR = Path(DIST_ROOT, "dist")
 SRC_DIR = Path(PROJECT_ROOT) / "flood_adapt"
 SITE_PACKAGES_PATH = Path(sys.executable).parent / "Lib" / "site-packages"
 
+# Dependencies to include in the executable
+DEPENDENCIES = [
+    "flood_adapt",
+    "rasterio",
+    "pyogrio",
+    "xugrid",
+    "hydromt",
+    "hydromt_fiat",
+    "hydromt_sfincs",
+    "scipy",
+]
+
 
 def copy_shapely_libs():
     # This has to be done now as the hook-shapely.py script is not updated to handle this apparently
-    shapely_dir = os.path.join(SITE_PACKAGES_PATH, "shapely")
+    shapely_dir = Path(SITE_PACKAGES_PATH, "shapely")
     if not os.path.exists(shapely_dir):
         raise FileNotFoundError("Shapely not found in site-packages")
 
-    shapely_libs_dir = os.path.join(SITE_PACKAGES_PATH, "Shapely.libs")
+    shapely_libs_dir = Path(SITE_PACKAGES_PATH, "Shapely.libs")
     if not os.path.exists(shapely_libs_dir):
         os.makedirs(shapely_libs_dir)
 
-    dlls = [
-        os.path.join(shapely_dir, "geos.dll"),
-        os.path.join(shapely_dir, "geos_c.dll"),
-    ]
-    for dll in dlls:
-        if not os.path.exists(dll):
-            raise FileNotFoundError(f"{dll} not found in shapely")
-        shutil.copy(src=dll, dst=shapely_libs_dir)
+    # The (outdated) hook for shapely expects the .dll files to be in the shapely.libs directory, so we have to copy them there
+    if not shapely_libs_dir.exists():
+        shapely_libs_dir.mkdir(exist_ok=True)
+        for file in shapely_dir.iterdir():
+            if file.suffix == ".dll" and "geos" in file.name:
+                shutil.copy2(src=file, dst=shapely_libs_dir)
 
 
 def run_pyinstaller() -> None:
@@ -55,17 +64,10 @@ def run_pyinstaller() -> None:
         f"--specpath={spec_path}",
     ]
 
-    command.append("--collect-all=flood_adapt")
-    command.append("--recursive-copy-metadata=flood_adapt")
-    command.append("--collect-all=rasterio")
-    command.append("--collect-all=pyogrio")
-    command.append("--collect-all=xugrid")
-    command.append("--collect-all=hydromt")
-    command.append("--recursive-copy-metadata=hydromt")
-    command.append("--collect-all=hydromt_fiat")
-    command.append("--recursive-copy-metadata=hydromt_fiat")
-    # command.append("--collect-all=hydromt_sfincs")
-    # command.append("--recursive-copy-metadata=hydromt_sfincs")
+    for dep in DEPENDENCIES:
+        command.append(f"--collect-all={dep}")
+        command.append(f"--recursive-copy-metadata={dep}")
+
     command.append(f"--paths={SITE_PACKAGES_PATH}")
     templates_path = entry_point.parent / "templates"
     command.append(f"--add-data={templates_path}:templates")
