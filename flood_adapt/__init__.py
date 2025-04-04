65a3f267a327e1103cbaf088dbc914022c339a43
Luuk Blom <luuk.blom@deltares.nl>
2025-04-04 13:23:20 +0200

    feat: add Site to __all__ in flood_adapt module for improved accessibility

--- flood_adapt/__init__.py
+++ flood_adapt/__init__.py
@@ -4,9 +4,11 @@ __version__ = "0.2.0"
 from flood_adapt import adapter, api, dbs_classes, object_model
 from flood_adapt.misc.config import Settings
 from flood_adapt.misc.log import FloodAdaptLogging
+from flood_adapt.object_model.interface.config.site import Site
 from flood_adapt.object_model.io import unit_system
 
 __all__ = [
+    "Site",
     "Settings",
     "FloodAdaptLogging",
     "unit_system",
