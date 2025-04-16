5ca31470c97f7eb73fae5f31589ca5348fbfc46e
Luuk Blom <luuk.blom@deltares.nl>
2025-04-16 15:11:57 +0200

    fix(db-builder): add setuptools to dependencies in build db-builder script

--- distribution/build_database_builder_executable.py
+++ distribution/build_database_builder_executable.py
@@ -1,14 +1,14 @@
 import os
 import shutil
-import subprocess
 import sys
 from pathlib import Path
 
 try:
     import PyInstaller.__main__  # noQA
-except Exception:
-    subprocess.check_call([sys.executable, "-m", "pip", "install", "pyinstaller==6.7"])
-    import PyInstaller.__main__  # noQA
+except ImportError as e:
+    raise ImportError(
+        "PyInstaller is not installed. Please install it using 'pip install .[build]'."
+    ) from e
 
 
 PROJECT_ROOT = Path(__file__).resolve().parents[1]
@@ -18,11 +18,13 @@ BUILD_DIR = Path(DIST_ROOT, "build")
 DIST_DIR = Path(DIST_ROOT, "dist")
 SRC_DIR = Path(PROJECT_ROOT) / "flood_adapt"
 SITE_PACKAGES_PATH = Path(sys.executable).parent / "Lib" / "site-packages"
+ENTRY_POINT = SRC_DIR / "database_builder" / "database_builder.py"
 
 # Dependencies to include in the executable
 DEPENDENCIES = [
     "flood_adapt",
     "rasterio",
+    "setuptools",
     "pyogrio",
     "xugrid",
     "hydromt",
@@ -51,17 +53,14 @@ def copy_shapely_libs():
 
 
 def run_pyinstaller() -> None:
-    entry_point = SRC_DIR / "database_builder" / "database_builder.py"
-    spec_path = DIST_ROOT
-
     command = [
-        str(entry_point),
+        str(ENTRY_POINT),
         "--clean",
         "--noconfirm",
         f"--name={PROJECT_NAME}",
         f"--workpath={BUILD_DIR}",
         f"--distpath={DIST_DIR}",
-        f"--specpath={spec_path}",
+        f"--specpath={DIST_ROOT}",
     ]
 
     for dep in DEPENDENCIES:
@@ -69,7 +68,7 @@ def run_pyinstaller() -> None:
         command.append(f"--recursive-copy-metadata={dep}")
 
     command.append(f"--paths={SITE_PACKAGES_PATH}")
-    templates_path = entry_point.parent / "templates"
+    templates_path = ENTRY_POINT.parent / "templates"
     command.append(f"--add-data={templates_path}:templates")
     PyInstaller.__main__.run(command)
 
