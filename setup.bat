bff1ebaa5c4ab8d4778e9fa366db022b36d0c282
Luuk Blom <luuk.blom@deltares.nl>
2025-05-16 11:20:17 +0200

    Update project configuration and add activation scripts for Windows and Linux to set the binaries env variables.

---
+++ setup.bat
@@ -0,0 +1,6 @@
+@echo off
+set SCRIPT_DIR=%~dp0
+for /f "usebackq delims=" %%i in (`powershell -NoProfile -Command "Resolve-Path -Path '%SCRIPT_DIR:~0,-1%\..\FloodAdapt\flood_adapt\system\win-64\sfincs\sfincs.exe'"`) do set SFINCS_BIN_PATH=%%i
+for /f "usebackq delims=" %%i in (`powershell -NoProfile -Command "Resolve-Path -Path '%SCRIPT_DIR:~0,-1%\..\FloodAdapt\flood_adapt\system\win-64\fiat\fiat.exe'"`) do set FIAT_BIN_PATH=%%i
+echo Set SFINCS_BIN_PATH to %SFINCS_BIN_PATH%
+echo Set FIAT_BIN_PATH to %FIAT_BIN_PATH%
