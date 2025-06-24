f88b68bd3eaa691896347f04b7a881e29c68b572
Panos Athanasiou <panos.athanasiou@deltares.nl>
2025-06-24 20:13:51 +0200

    update package structure

--- flood_adapt/__init__.py
+++ flood_adapt/__init__.py
@@ -4,7 +4,7 @@ __version__ = "0.3.12"
 from flood_adapt import adapter, dbs_classes, objects
 from flood_adapt.config.config import Settings
 from flood_adapt.config.site import Site
-from flood_adapt.database_builder.database_builder import DatabaseBuilder
+from flood_adapt.database_builder import create_database
 from flood_adapt.flood_adapt import FloodAdapt
 from flood_adapt.misc.exceptions import ComponentError, DatabaseError, FloodAdaptError
 from flood_adapt.misc.log import FloodAdaptLogging
@@ -22,7 +22,7 @@ __all__ = [
     "FloodAdaptError",
     "DatabaseError",
     "ComponentError",
-    "DatabaseBuilder",
+    "create_database",
 ]
 
 FloodAdaptLogging()  # Initialize logging once for the entire package
