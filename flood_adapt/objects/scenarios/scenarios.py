833a076ea6a7f6b39f15c542f83142d340062a55
Panos Athanasiou <panos.athanasiou@deltares.nl>
2026-02-25 11:04:13 +0100

    added validator in scenario to not allow empty stings

--- flood_adapt/objects/scenarios/scenarios.py
+++ flood_adapt/objects/scenarios/scenarios.py
@@ -1,7 +1,7 @@
-from flood_adapt.objects.object_model import Object
+from pydantic import BaseModel, ValidationInfo, field_validator
 
 
-class Scenario(Object):
+class Scenario(BaseModel):
     """BaseModel describing the expected variables and data types of a scenario.
 
     A scenario is a combination of an event, a projection, and a strategy, that all should be saved in the database.
@@ -9,14 +9,21 @@ class Scenario(Object):
     Attributes
     ----------
     event : str
-        The name of the event.
+        The name of the event. Must not be empty.
     projection : str
-        The name of the projection.
+        The name of the projection. Must not be empty.
     strategy : str
-        The name of the strategy.
+        The name of the strategy. Must not be empty.
 
     """
 
     event: str
     projection: str
     strategy: str
+
+    @field_validator("event", "projection", "strategy")
+    @classmethod
+    def validate_non_empty(cls, value: str, info: ValidationInfo) -> str:
+        if not value:
+            raise ValueError(f"The '{info.field_name}' attribute must not be empty.")
+        return value
